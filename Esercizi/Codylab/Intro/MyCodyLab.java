616615a0ad576718549542700f2b287c4c1eb653
Jacopo-Sassi <sassijacopp@gmail.com>
2025-05-27 12:56:58 +0200

    Creare una classe MyCodyLab che implementa il metodo messaggio che restituisce "Ciao"

---
+++ Esercizi/Codylab/Intro/MyCodyLab.java
@@ -0,0 +1,11 @@
+package Esercizi.Codylab.Intro;
+
+public class MyCodyLab {
+    public String messaggio() {
+        return("Ciao");
+    }
+    public static void main(String[]args) {
+        MyCodyLab mioOggetto =new MyCodyLab();
+        System.out.println(mioOggetto.messaggio());
+    }
+}
