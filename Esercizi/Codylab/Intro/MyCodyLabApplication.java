6e5594a5b33b424969bc2aaa28c74764c33a932a
Jacopo-Sassi <sassijacopp@gmail.com>
2025-05-27 12:36:16 +0200

    Creata una classe MyCodyLabApplication che implementa il metodo main che stampa a console "Hello CodyLab Talent 2025"

---
+++ Esercizi/Codylab/Intro/MyCodyLabApplication.java
@@ -0,0 +1,7 @@
+package Esercizi.Codylab.Intro;
+
+public class MyCodyLabApplication {
+    public static void main(String...args) {
+        System.out.println("Hello CodyLab Talent 2025");
+    }
+}
