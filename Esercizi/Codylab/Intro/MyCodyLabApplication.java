3a40bd85801b9a4520db18069dc64236193760c4
Jacopo-Sassi <sassijacopp@gmail.com>
2025-05-27 15:03:50 +0200

    Creare la classe Rettangolo che estende Quadrilatero, sovrascrive il costruttore ricevendo i dati base e altezza, e implementa il metodo area (base * altezza); Sovrascrivere il metodo toString perché scriva "Sono un rettangolo con base X e altezza Y" (dove X e Y sono i valori dei lati)

--- Esercizi/Codylab/Intro/MyCodyLabApplication.java
+++ Esercizi/Codylab/Intro/MyCodyLabApplication.java
@@ -4,5 +4,7 @@ public class MyCodyLabApplication {
     public static void main(String...args) {
         MyCodyLab MyCodyLab = new MyCodyLab();
         System.out.println( MyCodyLab.messaggio());
+        FormaGeometrica rettangolo = new Rettangolo(7,9);
+        System.out.println(rettangolo);
     }
 }
---
+++ Esercizi/Codylab/Intro/Rettangolo.java
@@ -0,0 +1,20 @@
+package Esercizi.Codylab.Intro;
+
+public class Rettangolo extends Quadrilatero {
+    private final float base, altezza;
+     public Rettangolo(float base, float altezza) {
+         super(base,altezza,base,altezza);
+         this.base = base;
+         this.altezza = altezza;
+     }
+
+    @Override
+    public float Area() {
+        return base*altezza;
+    }
+
+    @Override
+    public String toString() {
+        return "Sono un rettangolo con base" + base +"e altezza" + altezza;
+    }
+}
