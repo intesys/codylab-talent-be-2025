84bd21f4e0d9d22e5e7527bc65760d9e7bb11db1
Jacopo-Sassi <sassijacopp@gmail.com>
2025-05-27 17:09:06 +0200

    Creata una lista, utilizzato il metodo .stream e stampato i dati desiderati iterando gli elementi della lista

--- esercizi/codylab/intro/MyCodyLabApplication.java
+++ esercizi/codylab/intro/MyCodyLabApplication.java
@@ -1,5 +1,9 @@
 package esercizi.codylab.intro;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
 public class MyCodyLabApplication {
     public static void main(String...args) {
 
@@ -8,20 +12,33 @@ public class MyCodyLabApplication {
         FormaGeometrica quadrato = new Quadrato(5);
 
         FormaGeometrica cerchio = new Cerchio(5);
-        FormaGeometrica [] forme = {rettangolo,quadrato,cerchio};
-        stampaForme(forme);
+//        FormaGeometrica [] forme = {rettangolo,quadrato,cerchio};
+//        stampaForme(forme);
+
+        List <FormaGeometrica> formeUnite = new ArrayList<>();
+
+        formeUnite.add(cerchio);
+        formeUnite.add(rettangolo);
+        formeUnite.add(quadrato);
+
+        stampaForme(formeUnite);
     }
 
-    private static void stampaForme (FormaGeometrica [] forme) {
-//        for (int i = 0; i < forme.length; i++) {
-//            System.out.println(forme[i].toString());
-//            System.out.println(forme[i].perimetro());
-//            System.out.println(forme[i].area());
+    private static void stampaForme (List <FormaGeometrica> formeUnite) {
+        formeUnite.stream().forEach(formaGeometrica ->
+        {System.out.println(formaGeometrica.toString());
+        System.out.println(formaGeometrica.perimetro());
+        System.out.println(formaGeometrica.area());
+        });
+////        for (int i = 0; i < forme.length; i++) {
+////            System.out.println(forme[i].toString());
+////            System.out.println(forme[i].perimetro());
+////            System.out.println(forme[i].area());
+////        }
+//        for (FormaGeometrica f : forme) {
+//            System.out.println(f.toString());
+//            System.out.println(f.area());
+//            System.out.println(f.perimetro());
 //        }
-        for (FormaGeometrica f : forme) {
-            System.out.println(f.toString());
-            System.out.println(f.area());
-            System.out.println(f.perimetro());
-        }
     }
 }
