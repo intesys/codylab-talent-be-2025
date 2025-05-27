6e92b6eb6f2755a34fdbd70dfd42681c7358fb2f
saccomando-pietro <pietro.saccomando@gmail.com>
2025-05-27 12:09:39 +0200

    Prima classe Java

---
+++ it/intesys/codylab/MyCodyLabApplication.java
@@ -0,0 +1,8 @@
+package it.intesys.codylab;
+
+public class MyCodyLabApplication {
+    public static void main(String[] args) {
+        System.out.println("Welcome to My CodyLab Application");
+
+    }
+}
