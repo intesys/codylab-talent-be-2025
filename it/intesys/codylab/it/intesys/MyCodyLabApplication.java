9c2d6b0565a282d87d810f9cbd7e7180ac9a6adf
Gaetano <gaetano.zangari98@outlook.it>
2025-05-27 16:39:10 +0200

    Creazione di un ciclo while, creazione di una lista e di 2 metodi per scorrere la stessa (get e stream). Aggiunta di "Etichette" ai diversi cicli.

--- it/intesys/codylab/it/intesys/MyCodyLabApplication.java
+++ it/intesys/codylab/it/intesys/MyCodyLabApplication.java
@@ -1,4 +1,6 @@
 package it.intesys.codylab.it.intesys;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyCodyLabApplication {
 
@@ -10,15 +12,37 @@ public class MyCodyLabApplication {
         FormaGeometrica rettangolo = new Rettangolo(7, 3);
         FormaGeometrica quadrato = new Quadrato(7, 3, 4, 5);
         FormaGeometrica cerchio = new Cerchio(4);
-        FormaGeometrica[] forme = { rettangolo, quadrato, cerchio };
+        FormaGeometrica[] forme = {rettangolo, quadrato, cerchio};
+
+        System.out.println("\n########## FOR ########## \n");
+
         StampaForme(forme);
-        System.out.println("\n ######################################################\n");
+
+        System.out.println("\n########## WHILE ##########\n");
+
         StampaForme2(forme);
-        }
+
+        List<FormaGeometrica> nuoveForme = new ArrayList<>();
+        nuoveForme.add(rettangolo);
+        nuoveForme.add(quadrato);
+        nuoveForme.add(cerchio);
+
+        System.out.println("\n########## LISTA GET ##########\n");
+
+        StampaForme3(nuoveForme);
+
+        System.out.println("\n########## FOR 2 ##########\n");
+
+        StampaForme4(forme);
+
+        System.out.println("\n########## LISTA STREAM ##########\n");
+
+        StampaForme5(nuoveForme);
+
+    }
 
 
-        public static void StampaForme(FormaGeometrica[] forme)
-    {
+    public static void StampaForme(FormaGeometrica[] forme) {
         System.out.println("-------------------------------------------------");
         for (FormaGeometrica f : forme) {
             System.out.println(f);
@@ -26,17 +50,53 @@ public class MyCodyLabApplication {
             System.out.println("Il perimetro è " + f.perimetro());
             System.out.println("-------------------------------------------------");
         }
+
     }
 
-    public static void StampaForme2(FormaGeometrica[] forme)
-    {
+    public static void StampaForme2(FormaGeometrica[] forme) {
+        System.out.println("-------------------------------------------------");
+        int i = 0;
+        while(i < forme.length) {
+            System.out.println(forme[i]);
+            System.out.println("l'area è " + forme[i].area());
+            System.out.println("Il perimetro è " + forme[i].perimetro());
+            System.out.println("-------------------------------------------------");
+            i++;
+        }
+    }
+
+    public static void StampaForme3(List<FormaGeometrica> forme) {
+        System.out.println("-------------------------------------------------");
+        for (int i = 0; i < forme.size(); i++) {
+            System.out.println(forme.get(i));
+            System.out.println("l'area è " + forme.get(i).area());
+            System.out.println("Il perimetro è " + forme.get(i).perimetro());
+            System.out.println("-------------------------------------------------");
+        }
+    }
+
+
+    public static void StampaForme4(FormaGeometrica[] forme) {
         System.out.println("-------------------------------------------------");
         for (int i = 0; i < forme.length; i++) {
             System.out.println(forme[i]);
             System.out.println("l'area è " + forme[i].area());
             System.out.println("Il perimetro è " + forme[i].perimetro());
+            i++;
             System.out.println("-------------------------------------------------");
         }
-}
+    }
+
+
+    public static void StampaForme5(List<FormaGeometrica> forme) {
+        System.out.println("-------------------------------------------------");
+        forme.stream().forEach(f -> {
+            System.out.println(f);
+            System.out.println("L'area è " + f.area());
+            System.out.println("Il perimetro è " + f.perimetro());
+            System.out.println("-------------------------------------------------");
+        });
+    }
+
 }
 
