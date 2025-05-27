4e674686143b3a2a8a8a41937efa58144cfc9320
Gaetano <gaetano.zangari98@outlook.it>
2025-05-27 10:28:46 +0200

    main + classe

---
+++ it/intesys/codylab/MyCodyLabApplication.java
@@ -0,0 +1,13 @@
+package it.intesys.codylab;
+
+import it.intesys.codylab.it.intesys.MyCodyLab;
+
+public class MyCodyLabApplication {
+
+    public static void main(String[] args) {
+        MyCodyLab messaggio = null;
+        messaggio = new MyCodyLab();
+
+        System.out.println("CodyLab Application " + messaggio);
+    }
+}
---
+++ it/intesys/codylab/it/intesys/MyCodyLab.java
@@ -0,0 +1,8 @@
+package it.intesys.codylab.it.intesys;
+
+public class MyCodyLab {
+
+    public String mex(){
+    return "benvenuto!";
+    }
+}
