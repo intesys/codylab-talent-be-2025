bd4c189b04d8f806ea976c89ee30a3ea34d64096
Elisabetta Freschi <elisabetta.freschi@intesys.it>
2025-05-28 11:44:08 +0200

    Le dipendenze

--- src/main/java/it/intesys/codylab/business/StampaFormeGeometricheUseCase.java
+++ src/main/java/it/intesys/codylab/business/StampaFormeGeometricheUseCase.java
@@ -2,10 +2,14 @@ package it.intesys.codylab.business;
 
 import it.intesys.codylab.model.FormaGeometrica;
 import it.intesys.codylab.repository.FormaGeometricaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class StampaFormeGeometricheUseCase {
 
     private final FormaGeometricaRepository formaGeometricaRepository;
+    private static final Logger logger = LoggerFactory.getLogger(StampaFormeGeometricheUseCase.class);
 
     public StampaFormeGeometricheUseCase(FormaGeometricaRepository formaGeometricaRepository) {
         this.formaGeometricaRepository = formaGeometricaRepository;
@@ -16,9 +20,9 @@ public class StampaFormeGeometricheUseCase {
     }
 
     private void stampaPerimetroArea(FormaGeometrica formaGeometrica) {
-        System.out.println("Forma geometrica è " + formaGeometrica.toString());
-        System.out.println("perimetro: " + formaGeometrica.perimetro());
-        System.out.println("area: " + formaGeometrica.area());
+        logger.info("Forma geometrica è {}", formaGeometrica.toString());
+        logger.info("perimetro:{} ", formaGeometrica.perimetro());
+        logger.info("area: {}", formaGeometrica.area());
     }
 
 }
