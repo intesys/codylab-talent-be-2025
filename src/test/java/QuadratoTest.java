b8adfcda7f3d7fc3f519b59d05146ec6ae8c0fba
Gaetano <gaetano.zangari98@outlook.it>
2025-05-29 17:42:56 +0200

    Test sul quadrato e notifica di corretta esecuzione dal .jar.

--- src/test/java/QuadratoTest.java
+++ src/test/java/QuadratoTest.java
@@ -8,12 +8,6 @@ class QuadratoTest {
     @Test
     void testArea() {
         Quadrato q = new Quadrato(5);
-        assertEquals(25.0f, q.area(), 0.0001);
-    }
-
-    @Test
-    void testToString() {
-        Quadrato q = new Quadrato(3);
-        assertEquals("Quadrato con lato 3.0", q.toString());
+        assertEquals(25.0f, q.area());
     }
 }
