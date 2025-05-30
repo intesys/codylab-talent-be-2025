a3b646cc225ef317f53361f6e438e12acb644cc6
saccomando-pietro <pietro.saccomando@gmail.com>
2025-05-30 09:42:37 +0200

    Creazione package repository e interfaccia FormaGeometricaRepository

---
+++ src/main/java/it/intesys/codylab/repository/FormaGeometricaRepository.java
@@ -0,0 +1,8 @@
+package it.intesys.codylab.repository;
+import it.intesys.codylab.FormaGeometrica;
+
+import java.util.List;
+
+public interface FormaGeometricaRepository {
+    List<FormaGeometrica> findAll();
+}
