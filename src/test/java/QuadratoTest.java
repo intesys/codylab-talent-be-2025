54f302a1b7043387c2c54a28de0ec35dd5917417
Gaetano <gaetano.zangari98@outlook.it>
2025-05-30 10:12:39 +0200

    aggiunta "Test Scope" alla libreria logback.
    Inserimento di un blocco try - catch per stampare a LOG i risultati del test su perimetro ed area (AssertionError)

--- src/test/java/QuadratoTest.java
+++ src/test/java/QuadratoTest.java
@@ -1,19 +1,37 @@
 import it.intesys.codylab.Quadrato;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuadratoTest {
 
+    Logger logger = LoggerFactory.getLogger(QuadratoTest.class);
+
+
     @Test
     void testArea() {
         Quadrato q = new Quadrato(5);
-        assertEquals(25.0f, q.area());
+        try {
+            assertEquals(25.0f, q.area());
+            logger.info("Test Area superato correttamente.");
+        }
+        catch(AssertionError e){
+            logger.error("Errore durante il test dell'area.");
+            throw e;
+        }
     }
 
     @Test
     void testPerimetro() {
         Quadrato q = new Quadrato(5);
-        assertEquals(20.0f, q.perimetro());
+        try {
+            assertEquals(20.0f, q.perimetro());
+            logger.info("Test Perimetro superato correttamente.");
+        }
+        catch(AssertionError e){
+            logger.error("Errore durante il test del perimetro.");
+            throw e;
+        }
     }
 }
