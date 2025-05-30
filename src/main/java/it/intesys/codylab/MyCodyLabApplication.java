95a988fa7388f9c7651c61e66e1eb784b79b630f
Elisabetta Freschi <elisabetta.freschi@intesys.it>
2025-05-30 15:01:16 +0200

    Riferimento a business con collaboratore DummyFormaGeometricaRepository

--- src/main/java/it/intesys/codylab/MyCodyLabApplication.java
+++ src/main/java/it/intesys/codylab/MyCodyLabApplication.java
@@ -2,6 +2,7 @@ package it.intesys.codylab;
 
 import it.intesys.codylab.business.StampaFormeGeometricheUseCase;
 import it.intesys.codylab.repository.DataSourceFactory;
+import it.intesys.codylab.repository.DummyFormaGeometricaRepository;
 import it.intesys.codylab.repository.SqlFormaGeometricaRepository;
 
 public class MyCodyLabApplication {
@@ -12,7 +13,8 @@ public class MyCodyLabApplication {
     }
 
     private StampaFormeGeometricheUseCase initStampaFormeGeometricheUseCase() {
-        return new StampaFormeGeometricheUseCase(new SqlFormaGeometricaRepository(DataSourceFactory.makeDataSource()));
+        // return new StampaFormeGeometricheUseCase(new SqlFormaGeometricaRepository(DataSourceFactory.makeDataSource()));
+        return new StampaFormeGeometricheUseCase(new DummyFormaGeometricaRepository());
     }
 
 }
