5e70f6bf7e5e5bf7b493d70dbf0f27748c72b372
Jacopo-Sassi <sassijacopp@gmail.com>
2025-05-30 09:58:36 +0200

    Creare un test JUnit per Quadrato che verifica il calcolo di perimetro ed area

---
+++ src/test/java/QuadratoTest.java
@@ -0,0 +1,20 @@
+import it.intesys.codylab.Quadrato;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QuadratoTest {
+
+    @Test
+    public void testPerimetro(){
+        Quadrato quadrato = new Quadrato(5);
+        float risultato = quadrato.perimetro();
+        assertEquals(20,risultato);
+    }
+
+    @Test
+    public void testArea(){
+        Quadrato quadrato = new Quadrato(5);
+        float risultato = quadrato.area();
+        assertEquals(25,risultato);
+    }
+}
