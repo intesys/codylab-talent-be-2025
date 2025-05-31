9c47aac1869a9c6feba87696bd11f4f033ee30cb
Enrico Oliosi <enrico.oliosi@intesys.it>
2025-05-31 18:21:40 +0200

    Aggiunti commenti e operazioni CRUD per la tabella formageometrica

--- src/main/resources/codylab.sql
+++ src/main/resources/codylab.sql
@@ -1,3 +1,4 @@
+-- Questo file contiene le istruzioni SQL per creare e gestire una tabella di forme geometriche.
 create table formageometrica(
                       id identity primary key,
                       tipo varchar(50) not null,
@@ -5,7 +6,15 @@ create table formageometrica(
                       lato2 double
 );
 
+-- Inseriamo alcune forme geometriche
 insert into formageometrica (id, tipo, lato1, lato2) values (1, 'quadrato', 4, null);
 insert into formageometrica (id, tipo, lato1, lato2) values (2, 'rettangolo', 4, 6);
 insert into formageometrica (id, tipo, lato1, lato2) values (3, 'cerchio', 4, null);
 insert into formageometrica (id, tipo, lato1, lato2) values (4, 'rettangolo', 5, 10);
+
+-- Modifichiamo i lati di un quadrato e di un rettangolo
+update formageometrica set lato1 = 6 where id = 1;
+update formageometrica set lato1 = 8 where id = 2;
+
+-- Cancelliamo un rettangolo
+delete from formageometrica where id = 4;
