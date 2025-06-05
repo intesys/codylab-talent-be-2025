557f647ecddbbc519f669c936dd139aab9a73925
saccomando-pietro <pietro.saccomando@gmail.com>
2025-06-05 17:57:30 +0200

    Avviato servizi tramite docker-compose: docker-compose -f .\docker-compose.yml up -d --build Collegato l'ide al database postgresql e creato le tabelle Testato il main MyCodyLabApplion collegato alle tabelle postgresql su Docker

--- src/main/java/it/intesys/codylab/MyCodyLabApplication.java
+++ src/main/java/it/intesys/codylab/MyCodyLabApplication.java
@@ -9,25 +9,8 @@ public class MyCodyLabApplication {
     public static void main(String[] args) {
         FormeGeometricheService formeGeometricheService = new MyCodyLabApplication().initStampaFormeGeometricheUseCase();
 
-        // Salva una nuova forma geometrica
-        formeGeometricheService.save("rettangolo", 5.0, 3.0);
-        formeGeometricheService.save("quadrato", 4.0,  null);
-        formeGeometricheService.save("cerchio", 2.0, null);
-
         // Trova per ID
         formeGeometricheService.findById(1);
-
-        // Trova per nome
-        formeGeometricheService.findByNome("quadrato");
-
-        // Stampa tutte le forme geometriche (con perimetro e area)
-        formeGeometricheService.stampaFormeGeometriche();
-
-        //Aggiorna una forma geometrica
-        formeGeometricheService.update(1, 2, 1);
-
-        //Elimina una forma geometrica
-        formeGeometricheService.deleteByNome("cerchio");
     }
 
     private FormeGeometricheService initStampaFormeGeometricheUseCase() {
