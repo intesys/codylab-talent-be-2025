0f946ce0d11f6467194ed5a6880938102778f641
Elisabetta Freschi <elisabetta.freschi@intesys.it>
2025-06-05 16:09:59 +0200

    Configurazione con Bean e Qualifier

--- src/main/java/it/intesys/codylab/config/CodyLabAppConfiguration.java
+++ src/main/java/it/intesys/codylab/config/CodyLabAppConfiguration.java
@@ -2,7 +2,11 @@ package it.intesys.codylab.config;
 
 import it.intesys.codylab.business.FormeGeometricheService;
 import it.intesys.codylab.repository.DataSourceFactory;
+import it.intesys.codylab.repository.DummyFormaGeometricaRepository;
+import it.intesys.codylab.repository.FormaGeometricaRepository;
 import it.intesys.codylab.repository.SqlFormaGeometricaRepository;
+import jdk.jfr.Name;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +14,22 @@ import org.springframework.context.annotation.Configuration;
 public class CodyLabAppConfiguration {
 
     @Bean
-    public FormeGeometricheService getFormeGeometriceService() {
-        return new FormeGeometricheService(new SqlFormaGeometricaRepository(DataSourceFactory.makeDataSource()));
+    @Qualifier("sqlRepository")
+    public FormaGeometricaRepository createFormaGeometricaRepository() {
+        System.out.println("Creating SqlFormaGeometricaRepository");
+        return new SqlFormaGeometricaRepository(DataSourceFactory.makeDataSource());
+    }
+    @Bean
+    @Qualifier("dummyRepository")
+    public FormaGeometricaRepository createDummyFormaGeometricaRepository() {
+        System.out.println("Creating DummyFormaGeometricaRepository");
+        return new DummyFormaGeometricaRepository();
+    }
+
+    @Bean
+    public FormeGeometricheService getFormeGeometriceService(@Qualifier("sqlRepository")
+                                                                 FormaGeometricaRepository formaGeometricaRepository) {
+        System.out.println("getFormeGeometriceService con repo: " + formaGeometricaRepository);
+        return new FormeGeometricheService(formaGeometricaRepository);
     }
 }
