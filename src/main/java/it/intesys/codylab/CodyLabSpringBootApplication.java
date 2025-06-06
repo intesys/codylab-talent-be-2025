f3eae061eb526203743f4ceee6e83523c7939e6e
saccomando-pietro <pietro.saccomando@gmail.com>
2025-06-06 09:30:04 +0200

    Introdotto parent e Spring Boot 3.5.0 e dipendenza da spring-starter

---
+++ src/main/java/it/intesys/codylab/CodyLabSpringBootApplication.java
@@ -0,0 +1,11 @@
+package it.intesys.codylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodyLabSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CodyLabSpringBootApplication.class, args);
+    }
+}
