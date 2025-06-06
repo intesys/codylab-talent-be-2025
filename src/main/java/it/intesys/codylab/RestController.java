3f20eee2d987663a16343069d981813d78e88248
Jacopo-Sassi <sassijacopp@gmail.com>
2025-06-06 12:49:10 +0200

    Creato un RestController come sul branch principale

---
+++ src/main/java/it/intesys/codylab/RestController.java
@@ -0,0 +1,11 @@
+package it.intesys.codylab;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World";
+    }
+}
