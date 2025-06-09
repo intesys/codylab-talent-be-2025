a8dcc26ad72a5257a51d8acbcfbaa3875fcb3834
Gaetano <gaetano.zangari98@outlook.it>
2025-06-09 16:11:01 +0200

    Connessione effettuata tramite restController al database e corretta visualizzazione delle forme geometriche e dei loro dati all'indirizzo locale di SpringBoot

--- react-repo/cody-react/src/assets/components/Dati_Forme.tsx
+++ react-repo/cody-react/src/assets/components/Dati_Forme.tsx
@@ -1,12 +1,29 @@
-import Forme_Geometriche from "./Forme_Geometriche";
+import { useEffect, useState } from "react";
 
-function Dati_Forme() {
-    return(
-        <div className="dati-forme">
-            <h1>DATI:</h1>
-            <Forme_Geometriche/>
-        </div>
-    )
-}
 
-export default Dati_Forme;
+function Dati_Forme()
+    {
+    const [forma, setforma] = useState([]);
+
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/forme")
+            .then(response => response.json())
+            .then(data => setforma(data));
+    }, []);
+
+
+        return(
+            <div className="dati-forme">
+                <h1>DATI:</h1>
+                <ul>
+                    {forma.map(forma => (
+                        <li>{forma["lato1"]} --- {forma["lato2"]}</li>
+
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+
+    export default Dati_Forme;
--- react-repo/cody-react/src/assets/components/Nomi_Forme.tsx
+++ react-repo/cody-react/src/assets/components/Nomi_Forme.tsx
@@ -1,9 +1,23 @@
-import Forme_Geometriche from "./Forme_Geometriche";
+import { useEffect, useState } from "react";
 function Nomi_Forme() {
+    const [forma, setforma] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/forme")
+            .then(response => response.json())
+            .then(data => setforma(data));
+    }, []);
+
+
+
     return(
         <div className="nomi-forme">
             <h1>TIPO:</h1>
-            <Forme_Geometriche/>
+            <ul>
+                {forma.map(forma => (
+                    <li>{forma["id"]} {forma["tipo"]}</li>
+                ))}
+            </ul>
         </div>
     )
 }
