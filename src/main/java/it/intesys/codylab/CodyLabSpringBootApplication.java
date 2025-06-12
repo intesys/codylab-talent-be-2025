5ada1d614eb0a5d283052402b69a97e9379978b7
Jacopo-Sassi <sassijacopp@gmail.com>
2025-06-12 10:36:29 +0200

    Agganciato la dipendenza openapi e generare swagger e specificato openapi

--- src/main/java/it/intesys/codylab/CodyLabSpringBootApplication.java
+++ src/main/java/it/intesys/codylab/CodyLabSpringBootApplication.java
@@ -18,3 +18,5 @@ public class CodyLabSpringBootApplication {
         service.stampaFormeGeometriche();
     }
 }
+
+//
--- src/main/java/it/intesys/codylab/dto/WorkingHours.java
+++ src/main/java/it/intesys/codylab/dto/WorkingHours.java
@@ -2,11 +2,16 @@ package it.intesys.codylab.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.Duration;
 
 public class WorkingHours {
 
+    @Schema(
+            description = "Durata in formato ISO-8601 (es. PT1H30M per 1 ora e 30 minuti)",
+            example = "PT1H30M"
+    )
     private final Duration duration;
 
     @JsonCreator
