96cff3bba01ea6bf4cd1369a5a1547eaf6a29c9c
Enrico Oliosi <enrico.oliosi@intesys.it>
2025-06-12 17:56:45 +0200

    Rimozione vecchie classi

--- src/main/java/it/intesys/codylab/dto/UserProfile.java
+++
@@ -1,8 +0,0 @@
-package it.intesys.codylab.dto;
-
-public enum UserProfile {
-    PM, // Project Manager
-    SD, // Software Developer
-    JD, // Junior Developer
-    RT  // Release Technician
-}
