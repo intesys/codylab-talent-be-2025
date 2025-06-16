0d484cbf96cefbef2be41ea4735a0e20da953170
Jacopo-Sassi <sassijacopp@gmail.com>
2025-06-16 14:13:25 +0200

    Definita una CRUD Repository per Slot

---
+++ src/main/java/it/intesys/codylab/repository/SlotCrudRepository.java
@@ -0,0 +1,8 @@
+package it.intesys.codylab.repository;
+
+import it.intesys.codylab.model.Slot;
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface SlotCrudRepository extends ListCrudRepository<Slot, Long> {
+
+}
