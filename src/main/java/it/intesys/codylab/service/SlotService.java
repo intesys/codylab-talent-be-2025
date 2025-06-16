e9c1fb30700aa63b378cf18e01a464d58b6312af
Jacopo-Sassi <sassijacopp@gmail.com>
2025-06-16 14:15:42 +0200

    Modificato Slot Service sulla base della nuova interfaccia CRUD SlotCrudRepository

--- src/main/java/it/intesys/codylab/service/SlotService.java
+++ src/main/java/it/intesys/codylab/service/SlotService.java
@@ -1,20 +1,24 @@
 package it.intesys.codylab.service;
 
 import it.intesys.codylab.model.Slot;
+import it.intesys.codylab.repository.SlotCrudRepository;
 import it.intesys.codylab.repository.SlotRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SlotService {
-    private final SlotRepository slotRepository;
-    public SlotService(SlotRepository slotRepository) {
+    private final SlotCrudRepository slotRepository;
+
+    public SlotService(SlotCrudRepository slotRepository) {
         this.slotRepository = slotRepository;
     }
-    public Slot findById(Long slotId) {
-        return slotRepository.findById(slotId);
-    }
-    public void saveSlot(Slot slot) {
-        slotRepository.save(slot);
+
+    public Slot findById(Long id) {
+        return slotRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Slot not found with id: " + id));
     }
 
+    public Slot saveSlot(Slot slot) {
+        return slotRepository.save(slot);
+    }
 }
