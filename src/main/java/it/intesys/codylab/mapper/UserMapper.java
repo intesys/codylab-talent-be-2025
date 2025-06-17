7305920c8b6a6d5e5f4761fd3578da18d940226e
saccomando-pietro <pietro.saccomando@gmail.com>
2025-06-17 15:27:56 +0200

    aggiunti Mapper

---
+++ src/main/java/it/intesys/codylab/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package it.intesys.codylab.mapper;
+
+import it.intesys.codylab.dto.UserDTO;
+import it.intesys.codylab.model.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring", uses = TaskMapper.class)
+public interface UserMapper {
+    UserDTO toDTO(User user);
+
+    User toEntity(UserDTO userDTO);
+}
