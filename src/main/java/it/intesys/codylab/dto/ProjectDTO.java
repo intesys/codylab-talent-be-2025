c1bc9b54bad154df6acec012879110c919e0553b
saccomando-pietro <pietro.saccomando@gmail.com>
2025-06-17 15:14:10 +0200

    aggiunti DTO

---
+++ src/main/java/it/intesys/codylab/dto/ProjectDTO.java
@@ -0,0 +1,97 @@
+package it.intesys.codylab.dto;
+
+import java.util.List;
+
+public class ProjectDTO {
+
+    private Long id;
+    private String codice;
+    private String nome;
+    private String descrizione;
+    private String dataInizio;
+    private Integer durata;
+    private List<TaskDTO> tasks;
+
+    public ProjectDTO() {
+        //
+    }
+
+    public ProjectDTO(Long id, String codice, String nome, String descrizione, String dataInizio, Integer durata, List<TaskDTO> tasks) {
+        this.id = id;
+        this.codice = codice;
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.dataInizio = dataInizio;
+        this.durata = durata;
+        this.tasks = tasks;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodice() {
+        return codice;
+    }
+
+    public void setCodice(String codice) {
+        this.codice = codice;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getDataInizio() {
+        return dataInizio;
+    }
+
+    public void setDataInizio(String dataInizio) {
+        this.dataInizio = dataInizio;
+    }
+
+    public Integer getDurata() {
+        return durata;
+    }
+
+    public void setDurata(Integer durata) {
+        this.durata = durata;
+    }
+
+    public List<TaskDTO> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<TaskDTO> tasks) {
+        this.tasks = tasks;
+    }
+
+    @Override
+    public String toString() {
+        return "ProjectDTO{" +
+                "id=" + id +
+                ", codice='" + codice + '\'' +
+                ", nome='" + nome + '\'' +
+                ", descrizione='" + descrizione + '\'' +
+                ", dataInizio='" + dataInizio + '\'' +
+                ", durata=" + durata +
+                ", tasks=" + tasks +
+                '}';
+    }
+}
