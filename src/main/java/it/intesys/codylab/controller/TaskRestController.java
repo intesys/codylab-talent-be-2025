db7acf2ad4f27207a89afe520d855a2de9591625
saccomando-pietro <pietro.saccomando@gmail.com>
2025-06-19 14:07:09 +0200

    Creato metodo addUsersToTask per aggiungere utenti ad una task

--- src/main/java/it/intesys/codylab/controller/TaskRestController.java
+++ src/main/java/it/intesys/codylab/controller/TaskRestController.java
@@ -3,7 +3,10 @@ package it.intesys.codylab.controller;
 import it.intesys.codylab.dto.TaskDTO;
 import it.intesys.codylab.model.Task;
 import it.intesys.codylab.service.TaskService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -23,5 +26,11 @@ public class TaskRestController {
     @PostMapping("/task")
     public TaskDTO createTask(@RequestBody TaskDTO taskDTO) {
         return taskService.createTask(taskDTO);
+    }
+
+    @PostMapping("/task/{id}/users")
+    public ResponseEntity<TaskDTO> addUserToTask(@PathVariable Long id, @RequestBody List<Long> userIds) {
+        TaskDTO updated = taskService.addUsersToTask(id, userIds);
+        return ResponseEntity.ok(updated);
     }
 }
--- src/main/java/it/intesys/codylab/service/TaskService.java
+++ src/main/java/it/intesys/codylab/service/TaskService.java
@@ -4,25 +4,47 @@ import it.intesys.codylab.dto.TaskDTO;
 import it.intesys.codylab.mapper.TaskMapper;
 import it.intesys.codylab.model.Project;
 import it.intesys.codylab.model.Task;
+import it.intesys.codylab.model.User;
 import it.intesys.codylab.repository.ProjectRepository;
 import it.intesys.codylab.repository.TaskRepository;
+import it.intesys.codylab.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskService {
 
     private final TaskRepository taskRepository;
     private final ProjectRepository projectRepository;
+    private final UserRepository userRepository;
     private final TaskMapper taskMapper;
 
-    public TaskService(TaskRepository taskRepository, ProjectRepository projectRepository, TaskMapper taskMapper) {
+    public TaskService(TaskRepository taskRepository, ProjectRepository projectRepository, TaskMapper taskMapper, UserRepository userRepository) {
         this.taskRepository = taskRepository;
         this.projectRepository = projectRepository;
+        this.userRepository = userRepository;
         this.taskMapper = taskMapper;
     }
     public Task getTaskById(Long id) {
         return taskRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Task not found with id: " + id));
+    }
+
+    public TaskDTO addUsersToTask(Long taskId, List<Long> userIds) {
+        Task task = taskRepository.findById(taskId)
+                .orElseThrow(() -> new IllegalArgumentException("Task not found"));
+
+        List<User> users = (List<User>) userRepository.findAllById(userIds);
+
+        task.getUsers().addAll(users);
+
+        for (User user : users) {
+            user.getTasks().add(task);
+        }
+
+        Task saved = taskRepository.save(task);
+        return taskMapper.toDTO(saved);
     }
 
     public TaskDTO createTask(TaskDTO taskDTO) {
