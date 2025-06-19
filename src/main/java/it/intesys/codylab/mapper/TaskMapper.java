61c7209669d5220b79f2b4ba3cbe5d18b9e6b698
saccomando-pietro <pietro.saccomando@gmail.com>
2025-06-19 11:59:31 +0200

    Mappato id progetto nei task

--- src/main/java/it/intesys/codylab/mapper/TaskMapper.java
+++ src/main/java/it/intesys/codylab/mapper/TaskMapper.java
@@ -10,8 +10,10 @@ import org.mapstruct.Mapping;
 public interface TaskMapper {
 
     @Mapping(source = "dataInizio", target = "dataInizio", dateFormat = "dd-MM-yyyy")
+    @Mapping(source = "project.id", target = "projectId")
     TaskDTO toDTO(Task task);
 
     @Mapping(source = "dataInizio", target = "dataInizio", dateFormat = "dd-MM-yyyy")
+    @Mapping(source = "projectId", target = "project.id")
     Task toEntity(TaskDTO taskDTO);
 }
