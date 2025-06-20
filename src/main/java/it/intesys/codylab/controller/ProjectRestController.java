b75f40f87e8040dbe33f745d150b499ff2bdcb2d
saccomando-pietro <pietro.saccomando@gmail.com>
2025-06-20 11:40:03 +0200

    openapi - get /projects

--- src/main/java/it/intesys/codylab/controller/ProjectRestController.java
+++ src/main/java/it/intesys/codylab/controller/ProjectRestController.java
@@ -1,8 +1,12 @@
 package it.intesys.codylab.controller;
 
+import it.intesys.codylab.api.model.ProjectFilterApiDTO;
+import it.intesys.codylab.api.model.ProjectsApiDTO;
+import it.intesys.codylab.api.rest.ProjectsApi;
 import it.intesys.codylab.dto.ProjectDTO;
 import it.intesys.codylab.model.Project;
 import it.intesys.codylab.service.ProjectService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
-public class ProjectRestController {
+public class ProjectRestController implements ProjectsApi {
 
     private ProjectService projectService;
 
@@ -20,19 +24,31 @@ public class ProjectRestController {
         this.projectService = projectService;
     }
 
-    @GetMapping("/projects")
-    public List<ProjectDTO> getProjects() {
-        return projectService.findAll();
+    @Override
+    public ResponseEntity<List<ProjectsApiDTO>> getProjects(
+            Integer pageNumber,
+            Integer size,
+            String sort,
+            ProjectFilterApiDTO projectFilter) {
+        ProjectsApiDTO projectsApiDTO = new ProjectsApiDTO();
+        projectsApiDTO.setId(1L);
+        projectsApiDTO.setNome("Nome Progetto");
+        return ResponseEntity.ok(List.of(projectsApiDTO));
     }
 
-    @GetMapping("/project/{id}")
-    public Project getProjectById(@PathVariable Long id) {
-        return projectService.getProjectById(id);
-    }
-
-    @GetMapping("/project/codice/{codice}")
-    public ProjectDTO getProjectByCodice(@PathVariable String codice) {
-        return projectService.findByCodice(codice);
-    }
+    //    @GetMapping("/projects")
+//    public List<ProjectDTO> getProjects() {
+//        return projectService.findAll();
+//    }
+//
+//    @GetMapping("/project/{id}")
+//    public Project getProjectById(@PathVariable Long id) {
+//        return projectService.getProjectById(id);
+//    }
+//
+//    @GetMapping("/project/codice/{codice}")
+//    public ProjectDTO getProjectByCodice(@PathVariable String codice) {
+//        return projectService.findByCodice(codice);
+//    }
 
 }
