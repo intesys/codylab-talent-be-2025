28744bc375f801a1ab7644e679b3258086b586ae
Jacopo-Sassi <sassijacopp@gmail.com>
2025-06-26 10:56:23 +0200

    Creato filtro per cercare i progetti sulla base del loro codice

--- src/main/java/it/intesys/codylab/repository/ProjectRepository.java
+++ src/main/java/it/intesys/codylab/repository/ProjectRepository.java
@@ -9,10 +9,10 @@ import java.util.List;
 
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 
-    Project findByCodice(String codice);
 
     @Query("SELECT p FROM Project p FULL JOIN p.tasks t FULL JOIN t.users u WHERE u.id = :userId OR p.id IN :projectIds")
     List<Project> findByUserIdOrProjectIds(@Param("userId") Long userId, @Param("projectIds") List<Long> projectIds);
 
+    @Query("SELECT p FROM Project p WHERE (:codice IS NULL OR p.codice = :codice)")
+    List<Project> findByCodice(@Param("codice") String codice);
 }
-//d
