6331eed76288d68b0130ad4fc97220c56fdd678c
saccomando-pietro <pietro.saccomando@gmail.com>
2025-07-04 10:39:47 +0200

    Aggiunto @Param nei metodi in UserRepository

--- src/main/java/it/intesys/codylab/repository/UserRepository.java
+++ src/main/java/it/intesys/codylab/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,13 +15,13 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findUserWithProgettiResponsabile(Long id);
 
     @Query("SELECT u FROM User u JOIN u.tasks t WHERE t.id = :taskId AND u.id IN :ids")
-    Page<User> findByIdsAndTasksId(List<Long> ids, Long taskId, Pageable pageable);
+    Page<User> findByIdsAndTasksId(@Param("ids")List<Long> ids, @Param("taskId")Long taskId, Pageable pageable);
 
     @Query("SELECT u FROM User u JOIN u.tasks t WHERE t.id = :taskId")
-    Page<User> findByTasksId(Long taskId, Pageable pageable);
+    Page<User> findByTasksId(@Param("taskId")Long taskId, Pageable pageable);
 
     @Query("SELECT u FROM User u WHERE u.id IN :ids")
-    Page<User> findByIds(List<Long> ids, Pageable pageable);
+    Page<User> findByIds(@Param("ids")List<Long> ids, Pageable pageable);
 
 
 }
