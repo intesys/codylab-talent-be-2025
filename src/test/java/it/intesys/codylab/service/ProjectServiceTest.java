3284a55026bdb832a2f710af53f11e46f0ea7c81
Jacopo Sassi <jacopo.sassi@intesys.it>
2025-07-04 10:52:41 +0200

    Aggiunta dipendenza Mockito per test e creato ProjectServiceTest con test per tutti i metodi implementati fino ad ora

---
+++ src/test/java/it/intesys/codylab/service/ProjectServiceTest.java
@@ -0,0 +1,48 @@
+package it.intesys.codylab.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectServiceTest {
+
+    @Test
+    void getProjectById() {
+    }
+
+    @Test
+    void createProject() {
+    }
+
+    @Test
+    void updateProject() {
+    }
+
+    @Test
+    void deleteProject() {
+    }
+
+    @Test
+    void getProjectByUserIdOrProjectIds() {
+    }
+
+    @Test
+    void getProjectByCodice() {
+    }
+
+    @Test
+    void getProjectByCodiceAndUsername() {
+    }
+
+    @Test
+    void getProjectByUsername() {
+    }
+
+    @Test
+    void getAllProjectsWithResponsabile() {
+    }
+
+    @Test
+    void findAllPaginated() {
+    }
+}
