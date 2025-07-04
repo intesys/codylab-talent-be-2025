c93ed725ad4505f40d920686a6f44ccfd37382e9
Jacopo Sassi <jacopo.sassi@intesys.it>
2025-07-04 11:26:06 +0200

    Creato test getProjectById e test getProjectByIdNotFound

--- src/test/java/it/intesys/codylab/service/ProjectServiceTest.java
+++ src/test/java/it/intesys/codylab/service/ProjectServiceTest.java
@@ -1,48 +1,94 @@
 package it.intesys.codylab.service;
 
+import it.intesys.codylab.api.model.ProjectsApiDTO;
+import it.intesys.codylab.mapper.ProjectMapper;
+import it.intesys.codylab.model.Project;
+import it.intesys.codylab.repository.ProjectRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class ProjectServiceTest {
+        @Mock
+        ProjectRepository projectRepository;
+        @Mock
+        ProjectMapper projectMapper;
 
-    @Test
-    void getProjectById() {
-    }
+        @InjectMocks
+        ProjectService projectService;
 
-    @Test
-    void createProject() {
-    }
 
-    @Test
-    void updateProject() {
-    }
+        @DisplayName("Verifico che quando chiamo un progetto esistente torna dei dati consistenti")
+        @Test
+        void getProjectById() {
+            //ARRANGE
+            Project project = new Project();
+            project.setId(1L);
+            project.setCodice("PROJ123");
+            when(projectRepository.findById(1L)).thenReturn(Optional.of(project));
+            //when(projectMapper.toDTO(any(Project.class))).thenReturn(new ProjectDTO());
+            //ACT
+            ProjectsApiDTO projectResult = projectService.getProjectById(1L);
+            //ASSERT
+            assertNotNull(projectResult);
+            assertEquals(1l, projectResult.getId());
+            assertEquals("PROJ123", projectResult.getCodice());
+            verify(projectRepository, times(1)).findById(1L);
+        }
 
-    @Test
-    void deleteProject() {
-    }
+        @DisplayName("Verifico che quando chiamo un progetto NON esistente solleva una eccezione")
+        @Test
+        void getProjectByIdNotFound() {
+            //ARRANGE
+            when(projectRepository.findById(1L)).thenReturn(Optional.empty());
+            //when(projectMapper.toDTO(any(Project.class))).thenReturn(new ProjectDTO());
+            // ACT & ASSERT
+            RuntimeException exception = assertThrows(RuntimeException.class, () -> projectService.getProjectById(1L));
+            assertEquals("Project not found with id: 1", exception.getMessage());
+        }
 
-    @Test
-    void getProjectByUserIdOrProjectIds() {
-    }
-
-    @Test
-    void getProjectByCodice() {
-    }
-
-    @Test
-    void getProjectByCodiceAndUsername() {
-    }
-
-    @Test
-    void getProjectByUsername() {
-    }
-
-    @Test
-    void getAllProjectsWithResponsabile() {
-    }
-
-    @Test
-    void findAllPaginated() {
-    }
+//    @Test
+//    void createProject() {
+//    }
+//
+//    @Test
+//    void updateProject() {
+//    }
+//
+//    @Test
+//    void deleteProject() {
+//    }
+//
+//    @Test
+//    void getProjectByUserIdOrProjectIds() {
+//    }
+//
+//    @Test
+//    void getProjectByCodice() {
+//    }
+//
+//    @Test
+//    void getProjectByCodiceAndUsername() {
+//    }
+//
+//    @Test
+//    void getProjectByUsername() {
+//    }
+//
+//    @Test
+//    void getAllProjectsWithResponsabile() {
+//    }
+//
+//    @Test
+//    void findAllPaginated() {
+//    }
 }
