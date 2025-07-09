d9e6ffd3d8a1fd3d40ffacef0fd1aeef99534325
Gaetano Zangari <gaetano.zangari@intesys.it>
2025-07-09 15:20:03 +0200

    Aggiunta di configurazioni per H2 e Testcontainers, e creazione di test per il servizio ProjectService

---
+++ src/test/java/it/intesys/codylab/controller/TestController.java
@@ -0,0 +1,47 @@
+package it.intesys.codylab.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("h2")
+public class TestController {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void searchProjects_empty() throws Exception {
+        mockMvc.perform(get("/api/v1/projects"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    public void createProject()  throws Exception{
+        mockMvc.perform(post("/api/v1/projects")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"codice\":\"TEST001\",\"nome\":\"Test Project 001\",\"descrizione\":\"Test Description\"}"))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.nome").value("Test Project 001"));
+    }
+}
---
+++ src/test/java/it/intesys/codylab/service/ProjectServiceITest.java
@@ -0,0 +1,96 @@
+package it.intesys.codylab.service;
+
+import it.intesys.codylab.api.model.ProjectFilterApiDTO;
+import it.intesys.codylab.api.model.ProjectsApiDTO;
+import it.intesys.codylab.api.model.TasksApiDTO;
+import it.intesys.codylab.dto.ProjectDTO;
+import it.intesys.codylab.dto.TaskDTO;
+import it.intesys.codylab.model.Project;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class ProjectServiceITest {
+
+    @Autowired
+    ProjectService projectService;
+
+    @DisplayName("Ciclo di vita di un progetto")
+    @Test
+    @Transactional
+    void testLifeCycle() {
+        ProjectsApiDTO projectsApiDTO = new ProjectsApiDTO();
+        projectsApiDTO.setCodice("TEST001");
+        projectsApiDTO.setNome("TEST001");
+        projectsApiDTO.setDescrizione("Test Project 001");
+        TasksApiDTO tasksApiDTO = new TasksApiDTO();
+        tasksApiDTO.setCodice("TASK001");
+        tasksApiDTO.setNome("TASK001");
+        tasksApiDTO.setDescrizione("Test Task 001");
+
+        projectsApiDTO = projectService.createProject(projectsApiDTO);
+
+        assertNotNull(projectsApiDTO);
+        assertThat(projectsApiDTO.getId()).isNotNull();
+        assertThat(projectsApiDTO.getCodice()).isEqualTo("TEST001");
+
+        ProjectsApiDTO project = projectService.getProjectById(projectsApiDTO.getId());
+        assertNotNull(project);
+        assertThat(project.getCodice()).isEqualTo("TEST001");
+
+        projectsApiDTO = new ProjectsApiDTO();
+        projectsApiDTO.setCodice("TEST002");
+        projectsApiDTO.setNome("TEST002");
+        projectsApiDTO.setDescrizione("Test Project 002");
+        tasksApiDTO = new TasksApiDTO();
+        tasksApiDTO.setCodice("TASK002");
+        tasksApiDTO.setNome("TASK002");
+        tasksApiDTO.setDescrizione("Test Task 002");
+
+        projectService.createProject(projectsApiDTO);
+
+        ProjectFilterApiDTO filter = new ProjectFilterApiDTO();
+        Page<ProjectsApiDTO> allProjects = projectService.getProjects(filter, 0, 10, "id");
+        assertNotNull(allProjects);
+        assertThat(allProjects.getTotalElements()).isEqualTo(2);
+        for (ProjectsApiDTO projectApiDTO : allProjects.getContent()) {
+            System.out.println(projectApiDTO);
+        }
+    }
+
+    @Test
+    @Transactional
+    void findByCodice() {
+        ProjectsApiDTO projectsApiDTO = new ProjectsApiDTO();
+        projectsApiDTO.setCodice("TEST001");
+        projectsApiDTO.setNome("TEST001");
+        projectsApiDTO.setDescrizione("Test Project 001");
+        TasksApiDTO tasksApiDTO = new TasksApiDTO();
+        tasksApiDTO.setCodice("TASK001");
+        tasksApiDTO.setNome("TASK001");
+        tasksApiDTO.setDescrizione("Test Task 001");
+
+        projectsApiDTO = projectService.createProject(projectsApiDTO);
+
+        ProjectsApiDTO retrievedProject = projectService.getProjectById(projectsApiDTO.getId());
+        assertNotNull(retrievedProject);
+        assertThat(retrievedProject.getNome()).isEqualTo("TEST001");
+
+        ProjectFilterApiDTO filter = new ProjectFilterApiDTO();
+        filter.setProjectCodes(List.of("TEST000"));
+        Page<ProjectsApiDTO> projects = projectService.getProjects(filter, 0, 10, "id");
+        assertThat(projects.getTotalElements()).isEqualTo(0);
+    }
+}
