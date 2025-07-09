0ed1bf7bd856fa5589ab02948aab746809096b81
Pietro Saccomando <pietro.saccomando@intesys.it>
2025-07-09 15:29:39 +0200

    Aggiunti test di integrazione con testcontainer e H2

---
+++ src/test/java/it/intesys/codylab/service/H2ProjectServiceITest.java
@@ -0,0 +1,9 @@
+package it.intesys.codylab.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("h2")
+public class H2ProjectServiceITest extends ProjectServiceITest{
+}
---
+++ src/test/java/it/intesys/codylab/service/TestContainerProjectServiceITest.java
@@ -0,0 +1,9 @@
+package it.intesys.codylab.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("testcontainer")
+public class TestContainerProjectServiceITest extends ProjectServiceITest{
+}
