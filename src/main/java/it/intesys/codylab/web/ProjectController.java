104cdea81c0bfb74af00c1e7319f1045bf016471
Pietro Saccomando <pietro.saccomando@intesys.it>
2025-07-10 12:38:06 +0200

    Aggiunto metodo deleteProject in ProjectController

--- src/main/java/it/intesys/codylab/web/ProjectController.java
+++ src/main/java/it/intesys/codylab/web/ProjectController.java
@@ -2,15 +2,15 @@ package it.intesys.codylab.web;
 
 import it.intesys.codylab.api.model.ProjectFilterApiDTO;
 import it.intesys.codylab.api.model.ProjectsApiDTO;
-import it.intesys.codylab.model.Project;
 import it.intesys.codylab.service.ProjectService;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
 
 @Controller
 @RequestMapping("/mvc")
@@ -28,5 +28,11 @@ public class ProjectController {
         Page<ProjectsApiDTO> projects = projectService.getProjects(filter, 0, 10, null);
         model.addAttribute("projects", projects);
         return "projects";
+    }
+
+    @PostMapping("/project/delete/{id}")
+    public String deleteProject(@PathVariable Long id) {
+        projectService.deleteProject(id);
+        return "redirect:/mvc/projects";
     }
 }
