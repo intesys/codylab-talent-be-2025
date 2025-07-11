8ab8d98073ac26eab749bcc362af0bd48431dfe0
Jacopo Sassi <jacopo.sassi@intesys.it>
2025-07-11 16:41:57 +0200

    Refactor ProjectRestControllerITest: update test methods for project search and creation

--- src/test/java/it/intesys/codylab/controller/ProjectRestControllerITest.java
+++ src/test/java/it/intesys/codylab/controller/ProjectRestControllerITest.java
@@ -1,45 +1,48 @@
-//package it.intesys.codylab.controller;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import static org.hamcrest.Matchers.hasSize;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//@ActiveProfiles("test")
-//class ProjectRestControllerITest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Test
-//    void searchProjects() throws Exception {
-//        mockMvc.perform(get("/api/v1/projects"))
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.*", hasSize(1))) // Assuming at least one project exists
-//                .andExpect(jsonPath("$.[0].id").isNumber());
-//    }
-//
-//    @Test
-//    void createProject()  throws Exception{
-//        mockMvc.perform(post("/api/v1/projects")
-//                .contentType("application/json")
-//                .content("{\"code\":\"TEST001\",\"name\":\"Test Project 001\",\"description\":\"Test Description\"}"))
-//                .andDo(print())
-//                .andExpect(status().isCreated())
-//                .andExpect(jsonPath("$.id").value(1))
-//                .andExpect(jsonPath("$.name").value("Example Project"));
-//    }
-//}
+package it.intesys.codylab.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class ProjectRestControllerITest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void simpleSearchProjects() throws Exception {
+        mockMvc.perform(get("/api/v1/projects")
+                .param("search", "test"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    void createProject()  throws Exception{
+        String newProjectJson = "{ \"name\": \"New Project\", \"description\": \"A new project for testing\" }";
+
+        mockMvc.perform(post("/api/v1/projects")
+                .contentType("application/json")
+                .content(newProjectJson))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").value("New Project"))
+                .andExpect(jsonPath("$.description").value("A new project for testing"));
+    }
+}
