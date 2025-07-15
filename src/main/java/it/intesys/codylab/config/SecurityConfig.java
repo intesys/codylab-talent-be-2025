8326590116ad4351d449aa78237eb51e99d65244
Pietro Saccomando <pietro.saccomando@intesys.it>
2025-07-15 14:04:09 +0200

    Added basic security configuration. Added dependencies in pom.xml.

---
+++ src/main/java/it/intesys/codylab/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package it.intesys.codylab.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        try{
+            http.csrf(AbstractHttpConfigurer::disable)
+                    .authorizeHttpRequests(authorize -> authorize.requestMatchers("/api/v1/projects/**").authenticated().anyRequest().permitAll())
+                    .httpBasic(Customizer.withDefaults());
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+}
