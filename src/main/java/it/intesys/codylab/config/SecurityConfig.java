00dd913a8e88d84f33745295335784a38167ae53
Jacopo Sassi <sassijacopp@gmail.com>
2025-07-21 15:21:44 +0200

    Refactor security configuration: update endpoint authorization and simplify error handling

--- src/main/java/it/intesys/codylab/config/SecurityConfig.java
+++ src/main/java/it/intesys/codylab/config/SecurityConfig.java
@@ -2,31 +2,25 @@ package it.intesys.codylab.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .cors(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/v1/projects/**", "/api/v1/users/**").authenticated()
-                        .anyRequest().permitAll()
-                )
-                .oauth2Login(oauth -> oauth
-                        .defaultSuccessUrl("http://localhost:5173", true)
-                )
-                .exceptionHandling(ex -> ex
-                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-                );
-
+        try {
+            http
+                    .cors(Customizer.withDefaults()) // Use CORS configuration from WebConfig
+                    .csrf(AbstractHttpConfigurer::disable)// Disable CSRF for simplicity, not recommended for production
+                    .authorizeHttpRequests(authorize -> authorize.requestMatchers("/api/v1/ciao/**").authenticated()
+                            .anyRequest().permitAll())
+                    .oauth2Login(Customizer.withDefaults());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         return http.build();
-    }}
-
+    }
+}
