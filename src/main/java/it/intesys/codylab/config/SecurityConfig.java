bb5b7a12b0dc759f93ab1bed0f1b9cba0e78d767
Gaetano Zangari <gaetano.zangari@intesys.it>
2025-08-01 12:44:42 +0200

    Enhance security configuration: added OAuth2 resource server dependency, updated request authorization rules, and ensured proper handling of CORS and JWT authentication.

--- src/main/java/it/intesys/codylab/config/SecurityConfig.java
+++ src/main/java/it/intesys/codylab/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package it.intesys.codylab.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,11 +24,17 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
                 )
                 .exceptionHandling(exceptions -> exceptions
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 );
+
         return http.build();
     }
 
